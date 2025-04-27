7db538c8d3fb5526cbeebbcd0498c622845cfffa
yangjiandong <chunquedong@163.com>
2025-04-27 21:52:26 +0800

    update wase

--- wase/waseDemo.js
+++ wase/waseDemo.js
@@ -116,7 +116,7 @@ if (ENVIRONMENT_IS_NODE) {
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: C:\Users\YANGJI~1\AppData\Local\Temp\tmpqls4jlnv.js
+// include: C:\Users\YANGJI~1\AppData\Local\Temp\tmpj7kffxds.js
 
   Module['expectedDataFileDownloads'] ??= 0;
   Module['expectedDataFileDownloads']++;
@@ -293,25 +293,25 @@ Module['FS_createPath']("/", "res", true, true);
     }
 
     }
-    loadPackage({"files": [{"filename": "/res/Roboto-Regular.ttf", "start": 0, "end": 145348}, {"filename": "/res/image.png", "start": 145348, "end": 156564}, {"filename": "/res/style.himl", "start": 156564, "end": 156632}, {"filename": "/res/view.himl", "start": 156632, "end": 156918}], "remote_package_size": 156918});
+    loadPackage({"files": [{"filename": "/res/Roboto-Regular.ttf", "start": 0, "end": 145348}, {"filename": "/res/image.png", "start": 145348, "end": 156564}, {"filename": "/res/style.himl", "start": 156564, "end": 156632}, {"filename": "/res/view.himl", "start": 156632, "end": 156865}], "remote_package_size": 156865});
 
   })();
 
-// end include: C:\Users\YANGJI~1\AppData\Local\Temp\tmpqls4jlnv.js
-// include: C:\Users\YANGJI~1\AppData\Local\Temp\tmpr3xk4wqa.js
+// end include: C:\Users\YANGJI~1\AppData\Local\Temp\tmpj7kffxds.js
+// include: C:\Users\YANGJI~1\AppData\Local\Temp\tmpirft5wtz.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if (Module['$ww'] || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: C:\Users\YANGJI~1\AppData\Local\Temp\tmpr3xk4wqa.js
-// include: C:\Users\YANGJI~1\AppData\Local\Temp\tmpdl7ymh8p.js
+  // end include: C:\Users\YANGJI~1\AppData\Local\Temp\tmpirft5wtz.js
+// include: C:\Users\YANGJI~1\AppData\Local\Temp\tmp_d10p14c.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: C:\Users\YANGJI~1\AppData\Local\Temp\tmpdl7ymh8p.js
+  // end include: C:\Users\YANGJI~1\AppData\Local\Temp\tmp_d10p14c.js
 
 
 // Sometimes an existing Module object exists with properties
@@ -4670,6 +4670,7 @@ function isMobile() { if (/Mobi|Android|iPhone/i.test(navigator.userAgent)) { re
         var w = m.width;
         var h = m.fontBoundingBoxDescent + m.fontBoundingBoxAscent;
         if (w == 0) w = 1;
+        if (h == 0) h = 1;
         var p = out >> 2;
         Module.HEAPF32[p++] = w+1;
         Module.HEAPF32[p++] = w;
@@ -4695,6 +4696,7 @@ function isMobile() { if (/Mobi|Android|iPhone/i.test(navigator.userAgent)) { re
         var w = m.width;
         var h = m.fontBoundingBoxDescent + m.fontBoundingBoxAscent;
         if (w == 0) w = 1;
+        if (h == 0) h = 1;
         var p = out >> 2;
         Module.HEAPF32[p++] = w+1;
         Module.HEAPF32[p++] = w;
